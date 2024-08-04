ebe920c81b67c384938c3c4524edde969385d493
csae8092 <peter.andorfer@oeaw.ac.at>
2024-08-04 10:45:56 +0200

    places to libraries; manuscripts

--- denormalize.py
+++ denormalize.py
@@ -26,6 +26,27 @@ print(f"  saving INSTITUTIONS as {final_file}")
 with open(final_file, "w", encoding="utf-8") as f:
     json.dump(source_data, f, ensure_ascii=False, indent=2)
 
+print("LIBRARIES")
+final_file = os.path.join(DATA_FOLDER, "libraries.json")
+source_file = os.path.join(JSON_FOLDER, "libraries.json")
+with open(source_file, "r", encoding="utf-8") as f:
+    source_data = json.load(f)
+
+print("  - place")
+seed_file = os.path.join(JSON_FOLDER, "places.json")
+with open(seed_file, "r", encoding="utf-8") as f:
+    seed_data = json.load(f)
+for key, value in source_data.items():
+    old_values = value["place"]
+    new_values = []
+    for x in old_values:
+        new_values.append(seed_data[f"{x['id']}"])
+    value["place"] = new_values
+
+print(f"  saving LIBRARIES as {final_file}")
+with open(final_file, "w", encoding="utf-8") as f:
+    json.dump(source_data, f, ensure_ascii=False, indent=2)
+
 
 print("WORKS")
 final_file = os.path.join(DATA_FOLDER, "works.json")
@@ -67,5 +88,27 @@ for key, value in source_data.items():
     value["institutional_context"] = new_values
 
 print(f"  saving WORKS as {final_file}")
+with open(final_file, "w", encoding="utf-8") as f:
+    json.dump(source_data, f, ensure_ascii=False, indent=2)
+
+
+print("MANUSCRIPTS")
+final_file = os.path.join(DATA_FOLDER, "manuscripts.json")
+source_file = os.path.join(JSON_FOLDER, "manuscripts.json")
+with open(source_file, "r", encoding="utf-8") as f:
+    source_data = json.load(f)
+
+print("  - library")
+seed_file = os.path.join(DATA_FOLDER, "libraries.json")
+with open(seed_file, "r", encoding="utf-8") as f:
+    seed_data = json.load(f)
+for key, value in source_data.items():
+    old_values = value["library"]
+    new_values = []
+    for x in old_values:
+        new_values.append(seed_data[f"{x['id']}"])
+    value["library"] = new_values
+
+print(f"  saving MANUSCRIPTS as {final_file}")
 with open(final_file, "w", encoding="utf-8") as f:
     json.dump(source_data, f, ensure_ascii=False, indent=2)
